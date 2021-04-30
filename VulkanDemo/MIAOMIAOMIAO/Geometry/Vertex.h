d2f22f03af73f234cc354376e85f25d163a5037a
kawakawaDe <fdsam@hotmail.com>
2021-04-30 22:19:55 +0100

    updaing render pass

--- VulkanDemo/MIAOMIAOMIAO/Geometry/Vertex.h
+++ VulkanDemo/MIAOMIAOMIAO/Geometry/Vertex.h
@@ -14,7 +14,49 @@ namespace Rendering
 		glm::vec3 normal;
 		glm::vec2 texCoords;
 		int32_t materialId;
+		// It specifies the number of bytes between data entries and whether to move to the next data entry 
+		// after each vertex or after each instance.
+		static VkVertexInputBindingDescription GetBindingDescription()
+		{
+			VkVertexInputBindingDescription bindingDescription = {};
+			bindingDescription.binding = 0;
+			bindingDescription.stride = sizeof(Vertex);
+			bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+			return bindingDescription;
+		}
 
+		static std::array<VkVertexInputAttributeDescription, 4> GetAttributeDescriptions() {
+			std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
+			attributeDescriptions[0].binding = 0;
+			attributeDescriptions[0].location = 0;
+			attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+			attributeDescriptions[1].binding = 0;
+			attributeDescriptions[1].location = 1;
+			attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attributeDescriptions[1].offset = offsetof(Vertex, normal);
+
+			attributeDescriptions[2].binding = 0;
+			attributeDescriptions[2].location = 2;
+			attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+			attributeDescriptions[2].offset = offsetof(Vertex, texCoords);
+
+			attributeDescriptions[3].binding = 0;
+			attributeDescriptions[3].location = 3;
+			attributeDescriptions[3].format = VK_FORMAT_R32_SINT;
+			attributeDescriptions[3].offset = offsetof(Vertex, materialId);
+
+			return attributeDescriptions;
+		}
+
+		bool operator==(const Vertex& other) const
+		{
+			return position == other.position &&
+				normal == other.normal &&
+				texCoords == other.texCoords &&
+				materialId == other.materialId;
+		}
 	};
 }
 
