b1746287d27cab75c55396b57d3e2fe1bff91980
kawakawaDe <fdsam@hotmail.com>
2021-05-29 17:32:03 +0100

    Pipeline update

--- VulkanDemo/KAWAII/VulkanWrapper/GraphicPipeline.cpp
+++ VulkanDemo/KAWAII/VulkanWrapper/GraphicPipeline.cpp
@@ -1,52 +1,70 @@
 #include "GraphicPipeline.h"
 #include "Device.h"
 #include "PipelineLayout.h"
+#include "RenderPass.h"
 
 #include "../Common/Macro.h"
 #include "../Common/Logger.h"
 
 namespace RHI
 {
-	GraphicPipeline::GraphicPipeline(const std::shared_ptr<Device> pDevice, const VkGraphicsPipelineCreateInfo info)
-		: PipelineBase(pDevice), m_info(info)
+	GraphicPipeline::GraphicPipeline(const std::shared_ptr<Device>& pDevice,
+		const VkGraphicsPipelineCreateInfo info,
+		const std::vector<std::shared_ptr<ShaderModule>> shaders,
+		const std::shared_ptr<RenderPass> pRenderPass,
+		const std::shared_ptr<PipelineLayout> pPipelineLayout)
+		: PipelineBase(pDevice), 
+		m_info(info),
+		m_shaders(shaders),
+		m_pRenderPass(pRenderPass)
 	{
+		m_pPipelineLayout = pPipelineLayout;
+
+		m_info.renderPass = pRenderPass->GetDeviceHandle();
+		m_info.layout = pPipelineLayout->GetDeviceHandle();
+
+		std::vector<VkPipelineShaderStageCreateInfo> stages(shaders.size());
+		for (uint32_t i = 0; i < stages.size(); i++)
+		{
+			stages[i].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+			stages[i].stage = shaders[i]->GetShaderStage();
+			stages[i].module = shaders[i]->GetDeviceHandle();
+
+			char* pEntryName = new char[ENTRY_NAME_LENGTH];
+			strcpy_s(pEntryName, ENTRY_NAME_LENGTH, shaders[i]->GetEntryName().c_str());
+			stages[i].pName = pEntryName;
+		}
+
+		m_info.stageCount = stages.size();
+		m_info.pStages = stages.data();
+
 		if(!Init())
 			K_ERROR("Error Initialize Graphic Pipeline");
 	}
 
 	bool GraphicPipeline::Init()
 	{
-		// Shader 
-		m_shaderStageInfo.resize(m_info.stageCount);
-		for (uint32_t i = 0; i < m_info.stageCount; i++)
-			m_shaderStageInfo[i] = m_info.pStages[i];
-		m_info.pStages = m_shaderStageInfo.data();
+		// Blend stage
+		for (uint32_t i = 0; i < m_info.pColorBlendState->attachmentCount; i++)
+			m_blendStatesInfo.push_back(m_info.pColorBlendState->pAttachments[i]);
 
-		// Vertex Buffer
-		m_vertexBindingsInfo.resize(m_info.pVertexInputState->vertexBindingDescriptionCount);
-		for (uint32_t i = 0; i < m_info.pVertexInputState->vertexBindingDescriptionCount; i++)
-			m_vertexBindingsInfo[i] = m_info.pVertexInputState->pVertexBindingDescriptions[i];
+		m_blendCreateInfo = *m_info.pColorBlendState;
+		m_blendCreateInfo.pAttachments = m_blendStatesInfo.data();
+		m_info.pColorBlendState = &m_blendCreateInfo;
 
-		m_vertexAttributesInfo.resize(m_info.pVertexInputState->vertexAttributeDescriptionCount);
-		for (uint32_t i = 0; i < m_info.pVertexInputState->vertexAttributeDescriptionCount; i++)
-			m_vertexAttributesInfo[i] = m_info.pVertexInputState->pVertexAttributeDescriptions[i];
+		// Depth Stencil stage
+		m_depthStencilCreateInfo = *m_info.pDepthStencilState;
+		m_info.pDepthStencilState = &m_depthStencilCreateInfo;
 
-		// Vertex input
-		m_vertexInputCreateInfo = *m_info.pVertexInputState;
-		m_vertexInputCreateInfo.pVertexBindingDescriptions = m_vertexBindingsInfo.data();
-		m_vertexInputCreateInfo.pVertexAttributeDescriptions = m_vertexAttributesInfo.data();
-		m_info.pVertexInputState = &m_vertexInputCreateInfo;
-		
-		// Input assembly : what king of geometry to draw and if primitive restar shoul be enable
+		// Input state
 		m_assemblyCreateInfo = *m_info.pInputAssemblyState;
 		m_info.pInputAssemblyState = &m_assemblyCreateInfo;
 
-		// Multisampling
+		// Multisampling 
 		m_multiSampleCreateInfo = *m_info.pMultisampleState;
 		m_info.pMultisampleState = &m_multiSampleCreateInfo;
 
-		// Rasterizer : Takes the geometry that is shaped by the vertices from the vertex shader 
-		// and turns it into fragments to be colored by the fragment shader. 
+		// Rasterization
 		m_rasterizerCreateInfo = *m_info.pRasterizationState;
 		m_info.pRasterizationState = &m_rasterizerCreateInfo;
 
@@ -56,8 +74,8 @@ namespace RHI
 		m_viewportStateCreateInfo.pScissors = nullptr;
 		m_viewportStateCreateInfo.scissorCount = 1;
 		m_viewportStateCreateInfo.pViewports = nullptr;
-		m_info.pViewportState = &m_viewportStateCreateInfo;
 
+		// Dynamic stage
 		m_dynamicStates =
 		{
 			VK_DYNAMIC_STATE_VIEWPORT,
@@ -69,11 +87,27 @@ namespace RHI
 		m_dynamicStatesCreateInfo.pDynamicStates = m_dynamicStates.data();
 		m_info.pDynamicState = &m_dynamicStatesCreateInfo;
 
+		// Shader stage
+		m_shaderStageInfo.resize(m_info.stageCount);
+		for (uint32_t i = 0; i < m_info.stageCount; i++)
+			m_shaderStageInfo[i] = m_info.pStages[i];
+		m_info.pStages = m_shaderStageInfo.data();
 
-		// Blend State
-		for (uint32_t i = 0; i < m_info.pColorBlendState->attachmentCount; i++)
-			m_blendStatesInfo.push_back(m_info.pColorBlendState->pAttachments[i]);
+		// Vertex stage
+		m_vertexBindingsInfo.resize(m_info.pVertexInputState->vertexBindingDescriptionCount);
+		for (uint32_t i = 0; i < m_info.pVertexInputState->vertexBindingDescriptionCount; i++)
+			m_vertexBindingsInfo[i] = m_info.pVertexInputState->pVertexBindingDescriptions[i];
 
+		m_vertexAttributesInfo.resize(m_info.pVertexInputState->vertexAttributeDescriptionCount);
+		for (uint32_t i = 0; i < m_info.pVertexInputState->vertexAttributeDescriptionCount; i++)
+			m_vertexAttributesInfo[i] = m_info.pVertexInputState->pVertexAttributeDescriptions[i];
+
+		m_vertexInputCreateInfo = *m_info.pVertexInputState;
+		m_vertexInputCreateInfo.pVertexBindingDescriptions = m_vertexBindingsInfo.data();
+		m_vertexInputCreateInfo.pVertexAttributeDescriptions = m_vertexAttributesInfo.data();
+		m_info.pVertexInputState = &m_vertexInputCreateInfo;
+
+		// Init
 		if (!PipelineBase::Init(m_device, m_pPipelineLayout))
 			return false;
 
@@ -85,6 +119,27 @@ namespace RHI
 		VkPipeline pipeline;
 		CHECK_VK_ERROR(vkCreateGraphicsPipelines(m_device->GetDeviceHandle(), 0, 1, &m_info, nullptr, &pipeline));
 		return pipeline;
+	}
+
+	void GraphicPipeline::FillupPipelineCreateInfo(VkGraphicsPipelineCreateInfo& info, const std::vector<std::shared_ptr<ShaderModule>> shaders,
+		const std::shared_ptr<RenderPass>& pRenderPass, const std::shared_ptr<PipelineLayout>& pPipelineLayout)
+	{
+		info.renderPass = pRenderPass->GetDeviceHandle();
+		info.layout = pPipelineLayout->GetDeviceHandle();
+
+		std::vector<VkPipelineShaderStageCreateInfo> stages(shaders.size());
+		for (uint32_t i = 0; i < stages.size(); i++)
+		{
+			stages[i].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+			stages[i].stage = shaders[i]->GetShaderStage();
+			stages[i].module = shaders[i]->GetDeviceHandle();
+
+			char* pEntryName = new char[ENTRY_NAME_LENGTH];
+			strcpy_s(pEntryName, ENTRY_NAME_LENGTH, shaders[i]->GetEntryName().c_str());
+			stages[i].pName = pEntryName;
+		}
+		info.stageCount = (uint32_t)stages.size();
+		info.pStages = stages.data();
 	}
 
 	GraphicPipeline::~GraphicPipeline()
--- VulkanDemo/KAWAII/VulkanWrapper/GraphicPipeline.h
+++ VulkanDemo/KAWAII/VulkanWrapper/GraphicPipeline.h
@@ -6,11 +6,19 @@
 
 namespace RHI
 {
+	class ShaderModule;
+	class RenderPass;
+	class PipelineLayout;
+
 	// Stages of the graphics pipeline
 	class GraphicPipeline : public PipelineBase
 	{
 	public:
-		GraphicPipeline(const std::shared_ptr<Device> pDevice, const VkGraphicsPipelineCreateInfo info);
+		GraphicPipeline(const std::shared_ptr<Device>& pDevice,
+			const VkGraphicsPipelineCreateInfo info,
+			const std::vector<std::shared_ptr<ShaderModule>> shaders,
+			const std::shared_ptr<RenderPass> pRenderPass,
+			const std::shared_ptr<PipelineLayout> pPipelineLayout);
 
 		GraphicPipeline(const GraphicPipeline&) = delete;
 		GraphicPipeline& operator=(const GraphicPipeline&) = delete;
@@ -18,7 +26,18 @@ namespace RHI
 
 		~GraphicPipeline();
 
+		// Getters
+		const VkGraphicsPipelineCreateInfo& GetInfo() const { return m_info; }
+		std::shared_ptr<RenderPass> GetRenderPass() const { return m_pRenderPass; }
+		std::shared_ptr<ShaderModule> GetShader(ShaderModule::ShaderType type) const { return m_shaders[(uint32_t)type]; }
+		VkPipelineBindPoint GetPipelineBindingPoint() const override { return VK_PIPELINE_BIND_POINT_GRAPHICS; }
+		uint32_t GetSubpassIndex() const override { return m_info.subpass; }
+
 		virtual VkPipeline CreatePipeline() override;
+
+		// filling in the structure (Shader stages, Fixed-function state, Pipeline layout, RenderPass)
+		static void FillupPipelineCreateInfo(VkGraphicsPipelineCreateInfo& info, const std::vector<std::shared_ptr<ShaderModule>> shaders,
+			const std::shared_ptr<RenderPass>& pRenderPass, const std::shared_ptr<PipelineLayout>& pPipelineLayout);
 
 	private:
 		bool GraphicPipeline::Init();
@@ -32,12 +51,15 @@ namespace RHI
 
 		VkPipelineColorBlendStateCreateInfo m_blendCreateInfo; // blending state
 		VkPipelineVertexInputStateCreateInfo m_vertexInputCreateInfo; // vertex input
-		VkGraphicsPipelineCreateInfo m_info; // Pipeline Info
 		VkPipelineInputAssemblyStateCreateInfo m_assemblyCreateInfo; // Input assembly
 		VkPipelineViewportStateCreateInfo m_viewportStateCreateInfo; // Viewports
 		VkPipelineRasterizationStateCreateInfo m_rasterizerCreateInfo; // Rasterizer
 		VkPipelineMultisampleStateCreateInfo m_multiSampleCreateInfo; // Multisampling
 		VkPipelineDepthStencilStateCreateInfo m_depthStencilCreateInfo; // Depth and stencil testing
 		VkPipelineDynamicStateCreateInfo m_dynamicStatesCreateInfo; // Dynamic state
+
+		VkGraphicsPipelineCreateInfo m_info; // Pipeline Info
+		std::shared_ptr<RenderPass> m_pRenderPass;
+		std::vector<std::shared_ptr<ShaderModule>> m_shaders;
 	};
 }
